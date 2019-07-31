f2ae8ff8a4d6b837f10b4ebf89649d8e97e885f7
AmandaAArthur <amandaarthur@kubrickgroup.com>
2019-07-31 10:49:31 +0100

    Longest Spree Conseq

--- SQLQueryLongestConsequtiveCC.sql
+++ SQLQueryLongestConsequtiveCC.sql
@@ -25,11 +25,8 @@ WHERE datediff(dd, LagtransDateByClient, LeadtransDateByClient) = 1
 --ORDER BY LongestConsecutiveSpree desc -- acc 8316 14 days back to back 
 )
 SELECT DISTINCT
-l.account_id,
-d.client_id,
-l.LongestConsecutiveSpree, 
-(LongestConsecutiveSpree - avg(LongestConsecutiveSpree) over ())/ (STDEV(LongestConsecutiveSpree) OVER()) as outlierScore,
-	max(LongestConsecutiveSpree) OVER (PARTITION BY [transDate]) maxDaysPerClient,
+--l.account_id,
+--d.client_id,
 CASE 
 		 WHEN datediff(year,c.date_of_birth,l.transDate) < 20 THEN 10
 		 WHEN datediff(year,c.date_of_birth,l.transDate) < 30 THEN 20
@@ -44,11 +41,31 @@ CASE
 		 WHEN datediff(year,c.date_of_birth,l.transDate) < 120 THEN 110
 		 WHEN datediff(year,c.date_of_birth,l.transDate) < 130  THEN 120
 			ELSE 
-			-1
-		END ageBins
+			0
+		END agebins,
+SUM(l.LongestConsecutiveSpree) OVER (PARTITION BY  
+--(LongestConsecutiveSpree - avg(LongestConsecutiveSpree) over ())/ (STDEV(LongestConsecutiveSpree) OVER()) as outlierScore,
+--	max(LongestConsecutiveSpree) OVER (PARTITION BY [transDate]) maxDaysPerClient,
+CASE 
+		 WHEN datediff(year,c.date_of_birth,l.transDate) < 20 THEN 10
+		 WHEN datediff(year,c.date_of_birth,l.transDate) < 30 THEN 20
+		 WHEN datediff(year,c.date_of_birth,l.transDate) < 40 THEN 30
+		 WHEN datediff(year,c.date_of_birth,l.transDate) < 50  THEN 40
+		 WHEN datediff(year,c.date_of_birth,l.transDate) < 60  THEN 50
+		 WHEN datediff(year,c.date_of_birth,l.transDate) < 70  THEN 60
+		 WHEN datediff(year,c.date_of_birth,l.transDate) <  80 THEN 70
+		 WHEN datediff(year,c.date_of_birth,l.transDate) <  90 THEN 80
+		 WHEN datediff(year,c.date_of_birth,l.transDate) < 100 THEN 90
+		 WHEN datediff(year,c.date_of_birth,l.transDate) < 110 THEN 100
+		 WHEN datediff(year,c.date_of_birth,l.transDate) < 120 THEN 110
+		 WHEN datediff(year,c.date_of_birth,l.transDate) < 130  THEN 120
+			ELSE 
+			0
+		END) as ClientageBins
+	INTO dbo.StDevCalsNew
 FROM longestConsSpree l
 JOIN [kub_lf].[dbo].[disp] d ON d.account_id = l.account_id
 JOIN [kub_lf].[dbo].[CleanClientDetails] c ON d.client_id = c.client_id
 WHERE d.type = 'OWNER'
-ORDER BY LongestConsecutiveSpree desc
+--ORDER BY LongestConsecutiveSpree desc
 -- Longest SPree by client in age bin 50 
